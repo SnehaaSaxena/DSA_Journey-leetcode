f724d6fcaf5409b7d486071309eb1e24cc893740
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-01 20:26:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return binaryExpo(x,(long) n);
+    }
+    public double binaryExpo(double x, long n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+           return 1.0/ binaryExpo(x,-n);
+        }
+        if(n % 2 == 1){
+            return x* binaryExpo(x * x, (n-1) / 2);
+        }else{
+            return binaryExpo(x * x, n/2);
+        }
+    }
+}
+
+// Explanation
+//n%2==1 -> ans=ans*x and n=n-1 else x*x and n/2
+//2^10 -> func(4,5) -> 4*func(16,2) -> func(256,1) -> 256* func(65536,0) 
