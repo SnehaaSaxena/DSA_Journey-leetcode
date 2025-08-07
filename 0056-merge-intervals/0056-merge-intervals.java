1c6f468ef46b640aaa9c7a14f682bdc13e385ed8
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-07 15:03:01 +0530

    Time: 10 ms (21.09%), Space: 46.5 MB (72.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,27 +1,41 @@
 class Solution {
     public int[][] merge(int[][] arr) {
-        Arrays.sort(arr,Comparator.comparingDouble( o-> o[0]));
-        ArrayList<int  [] > list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            int start=arr[i][0],end=arr[i][1];
+        // APPROACH 1
+
+        //Arrays.sort(arr,Comparator.comparingDouble( o-> o[0]));
+        //ArrayList<int  [] > list=new ArrayList<>();
+        // for(int i=0;i<arr.length;i++){
+        //     int start=arr[i][0],end=arr[i][1];
             
-            if(!list.isEmpty() && end<=list.get(list.size()-1)[1])
-              continue;
-            for(int j=i+1;j<arr.length;j++){
-                if(end>=arr[j][0])
-                  end=Math.max(end,arr[j][1]);
-                else
-                  break;
-            }
-            int li[]=new int[]{start,end};
-            list.add(li);
-        }
+        //     if(!list.isEmpty() && end<=list.get(list.size()-1)[1])
+        //       continue;
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(end>=arr[j][0])
+        //           end=Math.max(end,arr[j][1]);
+        //         else
+        //           break;
+        //     }
+        //     int li[]=new int[]{start,end};
+        //     list.add(li);
+        // }
         // int intervals[][]=new int[list.size()][2];
         // for(int i=0;i<list.size();i++){
         //     for(int j=0;j<2;j++){
         //         intervals[i][j]=list.get(i)[j];
         //     }
         // }
-       return list.toArray(new int[list.size()][]);
+        //return list.toArray(new int[list.size()][]);
+
+        // APPROACH 2
+        ArrayList<int [] > list=new ArrayList<>();
+        Arrays.sort(arr,Comparator.comparingDouble(o->o[0]));
+
+        for(int i=0;i<arr.length;i++){
+            if(!list.isEmpty() && list.get(list.size()-1)[1]>=arr[i][0])
+             list.get(list.size()-1)[1]=Math.max(list.get(list.size()-1)[1],arr[i][1]);
+            else
+            list.add( new int[]{arr[i][0],arr[i][1]});
+        }
+        return list.toArray(new int[list.size()][]);
     }
 }
