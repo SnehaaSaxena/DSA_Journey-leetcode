1a0c9be31c77ad5a9eb4c9eabb7c039d5e428676
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-11 23:21:23 +0530

    Update 0055-jump-game.java

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,24 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int coverage=0,destination=nums.length-1,lastJumpIdx=0;
-        for(int i=0;i<nums.length;i++){
-           coverage=Math.max(coverage,nums[i]+i);
-           if(i==lastJumpIdx){
-             lastJumpIdx=coverage;
+        // int coverage=0,destination=nums.length-1,lastJumpIdx=0;
+        // for(int i=0;i<nums.length;i++){
+        //    coverage=Math.max(coverage,nums[i]+i);
+        //    if(i==lastJumpIdx){
+        //      lastJumpIdx=coverage;
 
-             if(coverage>=destination){
-                return true;
-             }
-           }
+        //      if(coverage>=destination){
+        //         return true;
+        //      }
+        //    }
+        // }
+        // return false;
+
+        // APPROACH 2 
+        int destination=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]+i>=destination)
+               destination=i;
         }
-        return false;
+      return destination == 0;
     }
 }
