81b4e3d477102d34734752b6df0488476092e848
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-11-21 00:11:39 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.76%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -54,27 +54,69 @@ class MyStack {
 
 
     /// ANOTHER METHOD
-    Queue<Integer> q;
+//     Queue<Integer> q;
+//     public MyStack() {
+//         q=new LinkedList<>();
+//     }
+
+//      public void push(int x) {
+//         q.add(x);
+//         for(int i=0;i<q.size()-1;i++) q.add(q.poll());
+//      }
+
+//       public int pop() {
+//          return q.poll();
+//       }
+
+//       public int top() {
+//          return q.peek();
+//       }
+
+//       public boolean empty() {
+//         return q.isEmpty();
+//       }
+
+
+
+/// ANOTHER METHOD
+    Queue<Integer> q1;
+    Queue<Integer> q2;
     public MyStack() {
-        q=new LinkedList<>();
+        q1=new LinkedList<>();
+        q2=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+       q1.add(x);
+    }
+    
+    public int pop() {
+       while(q1.size()>1){
+          q2.add(q1.poll());
+       }
+       int top=q1.poll();
+       Queue<Integer> temp=q1;
+       q1=q2;
+       q2=temp;
+       return top;
+    }
+    
+    public int top() {
+       while(q1.size()>1){
+           q2.add(q1.poll());
+       }
+       int top=q1.peek();
+       q2.add(q1.poll());
+       Queue<Integer> temp=q1;
+       q1=q2;
+       q2=temp;
+       return top;
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
     }
 
-     public void push(int x) {
-        q.add(x);
-        for(int i=0;i<q.size()-1;i++) q.add(q.poll());
-     }
-
-      public int pop() {
-         return q.poll();
-      }
-
-      public int top() {
-         return q.peek();
-      }
-
-      public boolean empty() {
-        return q.isEmpty();
-      }
 }
 
 /**
