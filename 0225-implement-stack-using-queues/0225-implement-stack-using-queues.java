5cd4ff2eb4b5936c73e9a249bb2b7bdf1543a262
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-11-21 00:00:30 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.76%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,56 +1,80 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
+    // Queue<Integer> q1;
+    // Queue<Integer> q2;
+    // public MyStack() {
+    //     q1=new LinkedList<>();
+    //     q2=new LinkedList<>();
+    // }
+    
+    // public void push(int x) {
+    //     if(!q1.isEmpty()){
+    //        q1.add(x);
+    //     }else{
+    //        q2.add(x);
+    //     }
+    // }
+    
+    // public int pop() {
+    //     int top=-1;
+    //     if(!q1.isEmpty()){
+    //       while(!q1.isEmpty()){
+    //          top=q1.remove();
+    //         if(q1.isEmpty()) break;
+    //         q2.add(top);
+    //       }
+    //     }else{
+    //         while(!q2.isEmpty()){
+    //              top=q2.remove();
+    //             if(q2.isEmpty()) break;
+    //             q1.add(top);
+    //         }
+    //     }
+    //     return top;
+    // }
+    
+    // public int top() {
+    //     int top=-1;
+    //     if(!q1.isEmpty()){
+    //       while(!q1.isEmpty()){
+    //          top=q1.remove();
+    //         q2.add(top);
+    //       }
+    //     }else{
+    //         while(!q2.isEmpty()){
+    //              top=q2.remove();
+    //             q1.add(top);
+    //         }
+    //     }
+    //     return top;
+    // }
+    
+    // public boolean empty() {
+    //     return q1.isEmpty() && q2.isEmpty();
+    // }
+
+
+    /// ANOTHER METHOD
+    Queue<Integer> q;
     public MyStack() {
-        q1=new LinkedList<>();
-        q2=new LinkedList<>();
+        q=new LinkedList<>();
     }
-    
-    public void push(int x) {
-        if(!q1.isEmpty()){
-           q1.add(x);
-        }else{
-           q2.add(x);
-        }
-    }
-    
-    public int pop() {
-        int top=-1;
-        if(!q1.isEmpty()){
-          while(!q1.isEmpty()){
-             top=q1.remove();
-            if(q1.isEmpty()) break;
-            q2.add(top);
-          }
-        }else{
-            while(!q2.isEmpty()){
-                 top=q2.remove();
-                if(q2.isEmpty()) break;
-                q1.add(top);
-            }
-        }
-        return top;
-    }
-    
-    public int top() {
-        int top=-1;
-        if(!q1.isEmpty()){
-          while(!q1.isEmpty()){
-             top=q1.remove();
-            q2.add(top);
-          }
-        }else{
-            while(!q2.isEmpty()){
-                 top=q2.remove();
-                q1.add(top);
-            }
-        }
-        return top;
-    }
-    
-    public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
-    }
+
+     public void push(int x) {
+        q.add(x);
+        for(int i=0;i<q.size()-1;i++) q.add(q.poll());
+     }
+
+      public int pop() {
+         return q.poll();
+      }
+
+      public int top() {
+         return q.peek();
+      }
+
+      public boolean empty() {
+        return q.isEmpty();
+      }
 }
 
 /**
